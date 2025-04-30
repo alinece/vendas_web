4b8801878a49c4b0985f1df40ae10c42a996ac2c
Aline Ceccon <aline.eli@viasoft.com>
2025-04-30 12:58:49 -0300

    atualiza relatório

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // outros configs aqui...
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  }
 });
